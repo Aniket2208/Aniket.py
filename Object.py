e35d96e3a6aa731d42db350b9b34f752a84d351b
Aniket2208 <aniketm2208@gmail.com>
2022-11-29 09:04:29 +0530

    Understanding self

--- Object.py
+++ Object.py
@@ -1,8 +1,28 @@
 class A:
-    pass
+
+    def __init__(self):
+        self.name = "Ani"
+        self.age = 22
+
+    def update(self):
+        self.age = 30
+
+    def compare(self, other):
+        if (a.age == b.age):
+            return True
+        else:
+            return False
 
 a = A()
 b = A()
+b.name = 'Danny'
+b.update()
 
-print(id(a))
-print(id(b))
+if a.compare(b):
+    print("Same age")
+else:
+    print("Different age")
+
+
+print(a.name, a.age)
+print(b.name, b.age)
