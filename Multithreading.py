9cd280d03318160303406f2fc639fd1a4e817e1b
Aniket2208 <aniketm2208@gmail.com>
2022-11-30 12:41:07 +0530

    Methods overriding

---
+++ Multithreading.py
@@ -0,0 +1,31 @@
+#from threading import *
+from threading import Thread
+from time import sleep
+
+
+class A(Thread):
+
+    def run(self):
+        for i in range(4):
+            print('Hi')
+            sleep(1)
+
+
+class B(Thread):
+
+    def run(self):
+        for i in range(4):
+            print('Hello')
+            sleep(1)
+
+t1 = A()
+t2 = B()
+
+t1.start()
+sleep(0.2)
+t2.start()
+
+t1.join()
+t2.join()
+
+print('End')
